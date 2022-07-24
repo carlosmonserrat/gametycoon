abcb1368f347edde2cf95ca93c229b7fa09a7f36
Carlos <inf.carlos@outlook.com>
2022-07-25 01:10:08 +0300

    GAMEDEV-3: sset local timer

--- src/components/views/startview/index.tsx
+++ src/components/views/startview/index.tsx
@@ -1,51 +1,54 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import CenterMarginLayout from "../../layout/centermarginlayout";
 import style from "./style.module.css"
 
 const StartView = () => {
 
-    const [timeId, setTimeId] = useState(0);
-    const [gameTime, setGameTime] = useState(5000);
-    const [remainingTime, setRemainingTime] = useState(0);
+    const [timeLeftState, setTimeLeftState] = useState(0)
+
+    const timeDelay = 5000
+
+    let startTime = 0
+    let timeLeft = 0
+    let timeId = 0
+
+    const endGame = () => {
+        alert(`game over! ${timeLeft}`)
+    }
 
     const startGame = () => {
-        alert("game!")
-    }
-
-    const getJob = () => {
-
-    }
-
-    const startGameTimer = () => {
-        let timeId: number = setTimeout(startGame, gameTime) as unknown as number
-        setTimeId(timeId)
+        startTime = new Date() as unknown as number
+        timeLeft = timeDelay
+       // setTimeLeftState(timeLeft)
+        timeId = setTimeout(endGame, timeDelay) as unknown as number
     }
 
     const pauseGameTimer = () => {
+        let currentTime = new Date() as unknown as number
+        timeLeft -= (currentTime - startTime)
         clearTimeout(timeId)
-        alert("game is freeze")
+        //  setTimeLeftState(timeLeft)
+        alert(`game is freeze ${timeLeft}`)
     }
 
     const resumeGameTimer = () => {
-
+        console.log(timeLeft)
+        startTime = new Date() as unknown as number
+        timeId = setTimeout(endGame, timeLeft) as unknown as number
     }
 
     return (
         <CenterMarginLayout>
-
             <h1>Serious dev</h1>
-
-          time:  {timeId}
-
+            time: {timeLeftState}
             <br/>
-
-            <button onClick={startGameTimer} className={style.button}>Start</button>
+            <button onClick={startGame} className={style.button}>Start</button>
             <br/>
             <br/>
-            <button onClick={pauseGameTimer} className={style.button}>get job</button>
+            <button onClick={pauseGameTimer} className={style.button}>pause</button>
+            <button onClick={resumeGameTimer} className={style.button}>resume</button>
         </CenterMarginLayout>
     )
 }
-
 
 export default StartView
